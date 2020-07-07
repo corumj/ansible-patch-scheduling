0c76682d5d5bf8a03610f65ad806b09968790c9c
Jerry Corum <corum@me.com>
2020-07-07 09:00:20 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # ansible-advanced-scheduling
 An advanced scheduling lookup to allow patching 18 days after patch tuesday
+
+## Instructions for using this lookup plugin
+
+You'll need to either copy the patch_day.py file into your own lookup_plugins folder in your ansible workspace or clone this and modify the test.yml to your particular task.
+
+Set the variable for the lookup:
+```
+vars:
+  offset: 18
+  patch_day: "{{ lookup('patch_day', '{{ offset }}') }}"
+```
+You can change the offset days to any number or '0' if you want to run the block on Patch Tuesday.
+
+In your playbook, use a block to set a conditional for a range of tasks.
+```
+  tasks:
+    - name: Run tasks if it's time 
+      block:
+        - debug: 
+            msg: "Hello World"
+      when: ansible_date_time['date'] == patch_day
+```
---
+++ lookup_plugins/patch_day.py
@@ -0,0 +1,65 @@
+# python 3 headers, required if submitting to Ansible
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+DOCUMENTATION = """
+  lookup: second_tuesday
+  author: Jerry Corum <jcorum@redhat.com>
+  version_added: "0.1"
+  short_description: return second tuesday of month
+  description:
+      - This lookup returns the second tuesday of the month.
+  options:
+    _terms:
+      description: offset example 18 will provide the date of the second tuesday plus 18 days
+      required: True
+  notes:
+    - only read in variable context
+"""
+from ansible.errors import AnsibleError, AnsibleParserError
+from ansible.plugins.lookup import LookupBase
+from ansible.utils.display import Display
+
+display = Display()
+
+
+class LookupModule(LookupBase):
+
+  def run(self, terms, variables=None, **kwargs):
+    # lookups in general are expected to both take a list as input and output a list
+    # this is done so they work with the looping construct 'with_'.
+    ret = []
+    for term in terms:
+      display.debug("Offset: %s" % term)
+      # Don't use print or your own logging, the display class
+      # takes care of it in a unified way.
+      display.vvvv(u"Second tuesday lookup using %s as offset days" % term)
+      try:
+        if term:
+          import calendar
+          from datetime import datetime
+          from datetime import timedelta
+
+          year = int(datetime.today().strftime("%Y"))
+          month = int(datetime.today().strftime("%m"))
+
+          c = calendar.Calendar(firstweekday=calendar.SUNDAY)
+
+          monthcal = c.monthdatescalendar(year,month)
+
+          second_tuesday = [day for week in monthcal for day in week if \
+                          day.weekday() == calendar.TUESDAY and \
+                          day.month == month][1]
+
+          patch_day = second_tuesday + timedelta(days=int(term)) 
+
+          ret.append(patch_day)
+        else:
+          # Always use ansible error classes to throw 'final' exceptions,
+          # so the Ansible engine will know how to deal with them.
+          # The Parser error indicates invalid options passed
+          raise AnsibleParserError()
+      except AnsibleParserError:
+        raise AnsibleError("could not calculate date with offset: %s" % term)
+
+    return ret
